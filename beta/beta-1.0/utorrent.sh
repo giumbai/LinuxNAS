e2cd7e2c6a155b809c5e16922f803d4e78d20061
Sergiu <giumsytec@gmail.com>
2014-11-23 17:04:18 +0200

    Update

---
+++ beta/beta-1.0/utorrent.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Version 1.0
+#This is the installer for Utorrent
+apt-get install libssl0.9.8:i386
+mv utorrent /opt/utorrent
+chmod -R 777 /opt/utorrent
+ln -s /opt/utorrent/utserver /usr/bin/utserver
+#This will install utorrent 64 edition
+#To start utorrent use utserver -settingspath /opt/utorrent/ &
+#This should be added to the system startup
+#Utorrent can be accessed at http://localhost:8080/gui/
