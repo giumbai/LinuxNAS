a5012f59c138a4d476735d1faa7aac9fe5efd17a
Sergiu <giumsytec@gmail.com>
2014-12-30 23:41:52 +0200

    Update

--- beta/beta-1.0/virtualbox.sh
+++ beta/beta-1.0/virtualbox.sh
@@ -12,7 +12,7 @@ echo "##########################################################################
 echo -e "\033[7;36m You need to set up the password 'pass', if you don't then you need to modify the conf file \033[0m"
 echo "##############################################################################"
 adduser --ingroup vboxusers vbox
-wget 'http://downloads.sourceforge.net/project/phpvirtualbox/phpvirtualbox-4.3-1.zip' -O phpvirtualbox.zip
+wget 'http://downloads.sourceforge.net/project/phpvirtualbox/phpvirtualbox-4.3-2.zip' -O phpvirtualbox.zip
 unzip phpvirtualbox.zip -d /var/www/html/
 mv /var/www/html/phpvirtualbox-4.3-1 /var/www/html/vbox
 mv -f config.php /var/www/html/vbox/config.php
